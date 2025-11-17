aa83942082797124090135588407afd00941625d
TLTechbender <myjoyhasarrived2004@gmail.com>
2025-11-17 18:41:25 +0100

    update: testing

--- src/controllers/auth/signin.ts
+++ src/controllers/auth/signin.ts
@@ -280,14 +280,14 @@ export const signIn = catchAsync(async (req: Request, res: Response) => {
        res.cookie("refreshToken", tokens.refreshToken, {
         httpOnly: true,
         secure: env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.cookie("accessToken", tokens.accessToken, {
         httpOnly: true,
         secure: env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: 'lax',
         maxAge: 15 * 60 * 1000,
     });
     return AppResponse(
