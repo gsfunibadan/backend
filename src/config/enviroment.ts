782bff97a88364ab2b54e443c8f96f4e5d68f84a
TLTechbender <myjoyhasarrived2004@gmail.com>
2025-11-17 14:58:39 +0100

    fix: environment config

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -11,15 +11,7 @@ const envSchema = z.object({
     EMAIL_SERVICE_URL: z.string().min(1, "EMAIL_SERVICE_URL  is required"),
     JWT_SECRET: z.string().min(32, "JWT_SECRET must be at least 32 characters"),
 
-    EMAIL_HOST: z.string().min(1, "EMAIL_HOST is required"),
-
-    EMAIL_SECURE: z
-        .string()
-        .transform((val) => val === "true")
-        .default(false),
-    EMAIL_USER: z.email("EMAIL_USER must be a valid email"),
-    EMAIL_PASSWORD: z.string().min(1, "EMAIL_PASSWORD is required"),
-
+   
     ACCESS_TOKEN_SECRET: z.string().min(32, "ACCESS_TOKEN_SECRET must be at least 32 characters"),
     REFRESH_TOKEN_SECRET: z.string().min(32, "REFRESH_TOKEN_SECRET must be at least 32 characters"),
     ACCESS_TOKEN_EXPIRY: z.string().default("15m"),
